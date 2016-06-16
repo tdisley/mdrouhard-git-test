c591d3d279e388205140df25f5e7ddfe728911c8
Meg Drouhard <meg.drouhard@gmail.com>
2016-06-16 09:46:49 -0700

    updated fib function in test.py

--- test.py
+++ test.py
@@ -1,12 +1,22 @@
 def print_hello_world():
     print("hello world!")
 
+# Generator Version
+# def fib(n):
+#     a, b = 0, 1
+#     for i in range(n):
+#         a, b = b, a + b
+#         yield a
+
 
 def fib(n):
+    fibs = []
     a, b = 0, 1
     for i in range(n):
         a, b = b, a + b
-        yield a
+        fibs.append(a)
+    return fibs
+
 
 
 print_hello_world()
